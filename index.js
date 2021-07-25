43e78ac38d8bbdd05c9e95387e6d16785844de89
EliasSchaut <eschaut@outlook.de>
2021-07-25 16:29:04 +0200

    add dbInit to index.js

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ helper.command_tree = command_tree
 // when the client is ready
 client.once('ready', () => {
     if (config.enable_database) {
-        const db_helper = require("./db/models/example_model")
-        db_helper.Tags.sync()
+        require("./db/dbObjects.js")
     }
     console.log('Ready!');
 });
